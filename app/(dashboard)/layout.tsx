d838c4e8cea892227137af1ab1ec4db122f90ad7
seanchen0123 <270061968@qq.com>
2024-12-12 11:45:39 +0800

    feat: implement dashboard layout and sidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,16 @@
+import DesktopSidebar from '@/components/DesktopSidebar'
 import { Separator } from '@/components/ui/separator'
 import React, { ReactNode } from 'react'
 
-const DashboardLayout = ({children}: {children: ReactNode}) => {
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex h-screen'>
-      
+    <div className="flex h-screen">
+      <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
-        <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
-          ScrapeFlow
-        </header>
+        <header className="flex items-center justify-between px-6 py-4 h-[50px] container">ScrapeFlow</header>
         <Separator />
-        <div className='overflow-auto'>
-          <div className="flex-1 container py-4 px-8 text-accent-foreground">
-            { children }
-          </div>
+        <div className="overflow-auto">
+          <div className="flex-1 container py-4 px-8 text-accent-foreground">{children}</div>
         </div>
       </div>
     </div>
