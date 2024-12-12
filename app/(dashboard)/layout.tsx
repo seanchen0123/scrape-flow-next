de816257f999cc12a29eafd73ad7ede8d98bebad
seanchen0123 <270061968@qq.com>
2024-12-12 15:14:06 +0800

    feat: implement user authentication functionality using Clerk Api

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import BreadcrumbHeader from '@/components/BreadcrumbHeader'
 import DesktopSidebar from '@/components/Sidebar'
 import { ThemeModeToggle } from '@/components/ThemeModeToggle'
 import { Separator } from '@/components/ui/separator'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import React, { ReactNode } from 'react'
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
@@ -11,8 +12,11 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
-          <div className='gap-1 flex items-center'>
+          <div className='gap-2 flex items-center'>
             <ThemeModeToggle />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
         </header>
         <Separator />
